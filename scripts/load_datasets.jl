3eacfb6dd481e32ff8d92ce4615a37903eb1b4a8
Jimmy <jimmy.dubuisson@gmail.com>
2016-12-24 16:14:54 +0200

    Arxiv dataset

--- scripts/load_datasets.jl
+++ scripts/load_datasets.jl
@@ -8,10 +8,10 @@ include("../graphs.jl")
 # loading and exporting datasets
 ###
 
-# amazon_0601, web_google
-dataset = "google"
+# amazon_0601, web_google, arxiv_hep-ph
+dataset = "arxiv"
 
-#if dataset == "amazon"
+if dataset == "amazon"
 	@info("loading Amazon_0601 graph")
 	g = adjlist(UInt32, is_directed=true)
 	load_adjacency_list_from_csv(UInt32, g, "../datasets/Amazon_0601/Amazon0601.txt")
@@ -29,12 +29,12 @@ dataset = "google"
 
 	write_mgs3_graph(core, "Amazon_0601_core")
 	write_mgs4_graph(core, rcore, "Amazon_0601_core")
-	serialize_to_jld(core, "core", "Amazon_0601_core")
+	#serialize_to_jld(core, "core", "Amazon_0601_core")
 	
 	write_mgs3_graph(rcore, "Amazon_0601_rcore")
 	write_mgs4_graph(rcore, core, "Amazon_0601_rcore")
-	serialize_to_jld(rcore, "rcore", "Amazon_0601_rcore")
-#elseif dataset == "google"
+	#serialize_to_jld(rcore, "rcore", "Amazon_0601_rcore")
+elseif dataset == "google"
 	@info("loading Web_Google graph")
 	g = adjlist(UInt32, is_directed=true)
 	load_adjacency_list_from_csv(UInt32, g, "../datasets/Web_Google/web-Google.txt")
@@ -52,9 +52,32 @@ dataset = "google"
 
 	write_mgs3_graph(core, "Web_Google_core")
 	write_mgs4_graph(core, rcore, "Web_Google_core")
-	serialize_to_jld(core, "core", "Web_Google_core")
+	#serialize_to_jld(core, "core", "Web_Google_core")
 	
 	write_mgs3_graph(rcore, "Web_Google_rcore")
 	write_mgs4_graph(rcore, core, "Web_Google_rcore")
-	serialize_to_jld(rcore, "rcore", "Web_Google_rcore")
-#end
+	#serialize_to_jld(rcore, "rcore", "Web_Google_rcore")
+elseif dataset == "arxiv"
+	@info("loading Arxiv_HEP-PH graph")
+	g = adjlist(UInt32, is_directed=true)
+	load_adjacency_list_from_csv(UInt32, g, "../datasets/Arxiv_HEP-PH/Cit-HepPh.txt")
+	@info("# vertices:", length(vertices(g)))
+	@info("# edges:", num_edges(g))
+
+	@info("getting core")
+	core,oni,noi = get_core(g)
+	@info("# vertices:", length(vertices(core)))
+	@info("# edges:", num_edges(core))
+
+	@info("getting reverse graph")
+	rcore = get_reverse_graph(core) 
+	@info("# edges (rcore):", num_edges(rcore))
+
+	write_mgs3_graph(core, "Arxiv_HEP-PH_core")
+	write_mgs4_graph(core, rcore, "Arxiv_HEP-PH_core")
+	#serialize_to_jld(core, "core", "Arxiv_HEP-PH_core")
+	
+	write_mgs3_graph(rcore, "Arxiv_HEP-PH_rcore")
+	write_mgs4_graph(rcore, core, "Arxiv_HEP-PH_rcore")
+	#serialize_to_jld(rcore, "rcore", "Arxiv_HEP-PH_rcore")
+end
